844c35347021141fee85285ddb1bd1a3a536b4e8
AlfinurS <aaalfinur87@gmail.com>
2025-05-22 13:26:05 +0300

    settings

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,7 @@ import {
   provideZoneChangeDetection,
   isDevMode,
   importProvidersFrom,
+  APP_INITIALIZER,
 } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { provideAnimations } from '@angular/platform-browser/animations';
@@ -13,7 +14,11 @@ import { provideServiceWorker } from '@angular/service-worker';
 import { routes } from './app.routes';
 import { authInterceptor } from '../auth/auth.interceptor';
 import { CookieService } from 'ngx-cookie-service';
+import { AuthApiService } from '../app/api/auth.api.service';
 
+export function appInitializerFactory(authService: AuthApiService) {
+  return () => authService.restoreSession();
+}
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
@@ -26,6 +31,12 @@ export const appConfig: ApplicationConfig = {
       enabled: !isDevMode(),
       registrationStrategy: 'registerWhenStable:30000',
     }),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: appInitializerFactory,
+      deps: [AuthApiService],
+      multi: true,
+    },
     CookieService,
   ],
 };
